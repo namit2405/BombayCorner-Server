a20e37dd30dbbfdc19545c5d552314241cf0e5a1
namit2405 <jainnamit34@gmail.com>
2025-07-05 03:59:11 +0530

    comment create superuser

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]; then
-    python manage.py createsuperuser --no-input
-fi
+# if [[ $CREATE_SUPERUSER ]]; then
+#     python manage.py createsuperuser --no-input
+# fi
