65f08a73c51b25a6ab57b83f7bda6ef5f009a769
namit2405 <jainnamit34@gmail.com>
2025-07-05 02:57:08 +0530

    changed manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'commerce.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'commerce.settings'
+    settings_module = 'commerce.commerce.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'commerce.commerce.settings'
     os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
 
     try:
