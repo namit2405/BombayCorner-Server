748f6905aa84d1e7cb4781ee69efc9eb3a1c7dc2
namit2405 <jainnamit34@gmail.com>
2025-07-05 04:27:47 +0530

    2 changes

--- commerce/app/urls.py
+++ commerce/app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
-urlpatterns = [
-   path('', views.home, name="home"), 
+urlpatterns = [ 
    path('signup/',views.SignupView.as_view(),name='signup'),
    path('login/',views.LoginView.as_view(),name='login'),
    path('logout/',views.LogoutView.as_view(),name='logout'),
