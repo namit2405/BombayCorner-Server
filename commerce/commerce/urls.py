b833124dc232f129b5440dc801e994ad35e861b1
namit2405 <jainnamit34@gmail.com>
2025-07-05 03:13:41 +0530

    urls in urls.py

--- commerce/commerce/urls.py
+++ commerce/commerce/urls.py
@@ -21,5 +21,5 @@ from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls'))
+    path('',include('commerce.app.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
